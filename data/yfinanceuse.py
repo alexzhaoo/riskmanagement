1ea1d4ea813b315ca56896e05c1fb6e722c454e8
Alexander Zhao <zhaotianxiao722@icloud.com>
2024-12-26 04:13:15 -0500

    using yfinance now, data scraping complete

---
+++ data/yfinanceuse.py
@@ -0,0 +1,60 @@
+
+import yfinance as yf
+import pandas as pd
+import numpy as np
+
+
+# tickers = ['AAPL' , 'MSFT', 'GOOG', 'AMZN', 'NVDA', 'TSLA']
+
+
+# stock_data = {}
+# for ticker in tickers:
+#     data = yf.download(ticker, start="2000-01-01", end="2023-12-31")
+#     data.reset_index(inplace=True)
+#     data.columns = data.columns.droplevel(0)  # Drop the multi-index level if present
+#     data.columns = ['Date', 'Close', 'High', 'Low', 'Open', 'Volume']  # Explicitly rename
+#     data['Ticker'] = ticker
+#     stock_data[ticker] = data
+
+
+
+# combined_data = pd.concat(stock_data.values())
+
+combined_data = pd.read_csv(r'data\testing.csv')
+
+combined_data['Returns'] = combined_data.groupby('Ticker')["Close"].pct_change()
+
+combined_data.dropna(inplace=True)
+
+
+
+latest_ipo_date = combined_data.groupby('Ticker')['Date'].min().max()
+filtered_data = combined_data[combined_data['Date'] >= latest_ipo_date]
+
+
+filtered_data = filtered_data[filtered_data['Returns'].abs() < 1]
+
+
+filtered_data['Date'] = pd.to_datetime(filtered_data['Date'])
+
+
+filtered_data.set_index('Date', inplace=True)
+
+
+filtered_data.to_csv(r'data\noncompoundeddailyreturns.csv', index = True)
+
+def compound_returns(returns):
+    return np.prod(1 + returns) - 1
+
+monthly_data = filtered_data.groupby('Ticker').resample('M').agg({
+    'Returns': compound_returns,  
+    'Close': 'last',
+    'Volume': 'sum'
+}).reset_index()
+
+
+monthly_data['Cumulative Return'] = monthly_data.groupby('Ticker')['Returns'].transform(lambda x: (1 + x).cumprod() - 1)
+
+monthly_data.set_index('Date', inplace=True)
+
+monthly_data.to_csv(r'data\monthlycompounded2010.csv', index=True)
